410cf2ecbe18ed9d10e22cba068a73cd0f0410b1
Deloris Thompson <deloris@echobind.com>
2023-09-05 14:51:34 -0400

    Adds work required for integrating with Plasmic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/global.css";
+import type { AppProps } from "next/app";
+// This is a placeholder for adding Directus
+// import { DataProvider } from "@plasmicapp/loader-nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // This is a placeholder for adding Directus, DO NOT DELETE
+    // <DataProvider
+    //   name="env"
+    //   data={{
+    //     DIRECTUS_URL: process.env.NEXT_PUBLIC_DIRECTUS_URL,
+    //   }}
+    // >
+    <Component {...pageProps} />
+    // </DataProvider>
+  );
 }
